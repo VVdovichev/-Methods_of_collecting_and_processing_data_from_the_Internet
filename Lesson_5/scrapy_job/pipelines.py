e505ae3bb3e54c3b7b9098455f9adcfda3666952
Вячеслав Вдовичев <74736148+VVdovichev@users.noreply.github.com>
2021-08-07 23:06:57 +0300

    Homework of Lesson 5/6.

---
+++ Lesson_5/scrapy_job/pipelines.py
@@ -0,0 +1,24 @@
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from pymongo import MongoClient
+
+MONGO_HOST = 'mongodb://localhost:27017/'
+DB_NAME = 'GB_Homework'
+
+
+class JobScraperPipeline:
+    def __init__(self):
+        client = MongoClient(MONGO_HOST)
+        self.db = client[DB_NAME]
+
+    def process_item(self, item, spider):
+        collection = self.db[spider.name]
+        if collection.count_documents(item) == 0:
+            collection.insert_one(item)
+        return item
