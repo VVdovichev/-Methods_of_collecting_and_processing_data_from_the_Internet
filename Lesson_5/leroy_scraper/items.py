1b497ec3061d832820c77e602c565add33eb5c30
Вячеслав Вдовичев <74736148+VVdovichev@users.noreply.github.com>
2021-08-07 23:06:57 +0300

    Homework of Lesson 5/6.

---
+++ Lesson_5/leroy_scraper/items.py
@@ -0,0 +1,24 @@
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from itemloaders import processors
+
+
+class LeroyScraperItem(scrapy.Item):
+    # define the fields for your item here like:
+    _id = scrapy.Field()
+    link = scrapy.Field()
+    images = scrapy.Field(
+        output_processor=processors.Identity()
+    )
+    title = scrapy.Field()
+    price = scrapy.Field(
+        input_processor=processors.Compose(lambda value: [v.replace(' ', '') for v in value])
+    )
+    features = scrapy.Field(
+        output_processor=processors.Identity()
+    )
---
+++ Lesson_5/leroy_scraper/pipelines.py
@@ -0,0 +1,66 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import pathlib
+import scrapy
+from collections import defaultdict
+from pymongo import MongoClient
+from scrapy.pipelines.images import ImagesPipeline
+from settings import IMAGES_STORE
+
+MONGO_HOST = 'mongodb://localhost:27017/'
+DB_NAME = 'GB_Homework'
+
+
+class LeroyDataScraperPipeline:
+    def __init__(self):
+        client = MongoClient(MONGO_HOST)
+        self.db = client[DB_NAME]
+        self.base_path = pathlib.Path(IMAGES_STORE).absolute()
+
+    def parse_item(self, product_item) -> dict:
+        """ Prepare item for adding to database """
+        item = defaultdict(object)
+
+        for key, value in product_item.items():
+            if key == 'images':
+                item[key] = self.base_path.joinpath(product_item['_id']).as_posix()
+            elif key == 'features':
+                for feat_key, feat_value in product_item[key]:
+                    item[feat_key] = feat_value
+            else:
+                item[key] = value
+        return item
+
+    def process_item(self, product_item, spider):
+        """ Add item to database """
+        item = self.parse_item(product_item)
+        collection = self.db[spider.name]
+        if collection.count_documents(item) == 0:
+            collection.insert_one(item)
+        return item
+
+
+class LeroyImagesScraperPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        if not (images := item['images']):
+            return
+        for image in images:
+            try:
+                yield scrapy.Request(image)
+            except Exception as e:
+                print(f'Images not found! {e}')
+
+    def item_completed(self, results, item, info):
+        if results:
+            item['images'] = [img[1] for img in results if img[0]]
+        return item
+
+    def file_path(self, request, response=None, info=None, *, item=None):
+        folder = pathlib.Path().joinpath(item['_id'])
+        filename = pathlib.Path(request.url).name
+        return folder.joinpath(filename).as_posix()
